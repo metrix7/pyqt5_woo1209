d430a09522563612bf8a5464c639bc5aa869a75f
DESKTOP-9O49SC7\user <metrix765@gmail.com>
2020-12-09 19:22:42 +0900

    qt디자이너 코딩 직전에 커밋

--- main.py
+++ main.py
@@ -1,16 +1,44 @@
-# This is a sample Python script.
+import sys
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
 # Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+# if __name__ == '__main__'
+
+class MyWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setGeometry(100, 200, 300, 400)
+        self.setWindowTitle('TICKER')
+        self.setWindowIcon(QIcon('iconfinder_coin-9_3338901.png'))
+
+        btn = QPushButton('버튼1', self)
+        btn.move(10, 10)
+        btn.clicked.connect(self.btn_clicked)
+
+        btn2 = QPushButton('버튼2', self)
+        btn2.move(10, 40)
+        btn2.clicked.connect(self.btn_clicked)
+
+    def btn_clicked(self):
+        print("버튼클릭!")
+
+app = QApplication(sys.argv)
+
+window = MyWindow()
+window.show()
+
+
+
+# label = QLabel('hello')
+# label.show()
+# btn = QPushButton('hello')
+# btn.show()
+
+
+app.exec_()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
